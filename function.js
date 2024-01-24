6358003b54cf40a7d5bef2e7b9fb078a25a032ff
Jun <jun_hong.kuang23@student.cart.org>
2024-01-24 14:50:39 -0800

    js

--- function.js
+++ function.js
@@ -1,6 +1,18 @@
-function calculator(form) {
-    
-    let calc = form.math.value;
-    console.log(calc); 
+function calculator() {
+    let calc = document.getElementsByClassName(`math`)[0].value;
+    let cat = eval(calc);
+    document.open();
+    document.write(
+        `<form action="#">
+    <label for="Calc">Calculator</label>
+    <input type="Equation" id="math" class="math">
+    <button onclick="calculator();">Solve</button>
+    <div onload="calculator"></div>
+    </form>`)
+    document.write(
+        `<p>Result:</p>`
+    );
+    document.write(cat);
+    document.close();
 }
-calculator();
+
