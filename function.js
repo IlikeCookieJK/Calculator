64d934542adc69d81eb3216d2e248b371aef03bc
Jun <jun_hong.kuang23@student.cart.org>
2024-01-25 15:07:09 -0800

    JS

--- function.js
+++ function.js
@@ -1,14 +1,37 @@
 function calculator() {
-    let calc = document.getElementsByClassName(`math`)[0].value;
-    let cat = eval(calc);
+    let c1 = document.getElementsByClassName(`math1`)[0].value;
+    let c2 = document.getElementsByClassName(`math2`)[0].value;
+    let c3 = document.getElementsByClassName(`math3`)[0].value;
+    switch (c2) {
+        case (`+`):
+            cat = (`${(parseFloat(c1) + parseFloat(c3))}`);
+            break;
+        case (`-`):
+            cat = (`${(parseFloat(c1) - parseFloat(c3))}`);
+            break;
+        case (`*`):
+            cat = (`${(parseFloat(c1) * parseFloat(c3))}`);
+            break;
+        case (`/`):
+            cat = (`${(parseFloat(c1) / parseFloat(c3))}`);
+            break;
+        case (`%`):
+            cat = (`${(parseFloat(c1) % parseFloat(c3))}`);
+            break;
+        case (`^`):
+            cat = (`${(parseFloat(c1) ** parseFloat(c3))}`);
+            break;
+    }
     document.open();
     document.write(
         `<form action="#">
-    <label for="Calc">Calculator</label>
-    <input type="Equation" id="math" class="math">
-    <button onclick="calculator();">Solve</button>
-    <div onload="calculator"></div>
-    </form>`)
+        <label for="Calc">Calculator</label>
+        <input type="Equation" id="math" class="math1">
+        <input type="Equation" id="math" class="math2">
+        <input type="Equation" id="math" class="math3">
+        <button onclick="calculator();">Solve</button>
+        <div onload="calculator"></div>
+        </form>`)
     document.write(
         `<p>Result:</p>`
     );
